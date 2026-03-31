91cac7b94013561104448a95bc6f5accd9963ec9
adilrakhimbekov2026-star <adirakhimbekov.2026@gmail.com>
2026-03-31 18:24:35 +0500

    Fix case sensitivity issues and duplicate folders

--- Frontend/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next';
-import { useBalance } from 'wagmi';
-import {ConnectButton} from '@rainbow-me/rainbowkit';
-export default function Home() {
-  return (
-    <div >
-    </div>
-  )
-}
--- backend/connect.py
+++ backend/connect.py
@@ -5,3 +5,5 @@ app=FastAPI()
 @app.get('/')
 async def main():
     return {'message': 'hello world'}
+
+# dont change the port (8000) for caddy to work
